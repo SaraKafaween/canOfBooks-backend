2196e36f4ecb978c02e6782136dcbdb61e3610a4
sara kafaween <sarakafaween@yahoo.com>
2021-09-26 09:08:29 +0300

    lab12

---
+++ models/book.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String },
+    description:{type: String},
+    status: { type: String },
+
+
+});
+
+
+module.exports = bookSchema;
--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa'); // we are going to use this package to connect to Auth0
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 const JWKSURI = process.env.JWKSURI;
-app.use(cors())
+const MONGO_DB_URL =process.env.MONGO_DB_URL;
+const {seedUserData} = require('./models/user.model');
+const getBooks = require('./controller/book.controller');
+app.use(cors());
+
+mongoose.connect(`${MONGO_DB_URL}books`, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const client = jwksClient({
   jwksUri: JWKSURI
@@ -34,6 +41,23 @@ jwt.verify(token, getKey, {}, (error, user) =>{ // pass it to the auth to check 
   response.json(user);//send user information in the state from auth
 });
 });
+
+seedUserData();
+
+app.get('/books',getBooks);
+
+
+
+
+
+ 
+  // TODO: 
+  // STEP 1: get the jwt from the headers
+  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
+  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+
+
 
   app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
